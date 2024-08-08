09a79b49bc944f5a0a62b503ec13a5556b5e06ca
Tylor Clont <admin@Tylors-Mac-mini.local>
2024-08-08 14:32:25 -0400

    f-string | .upper() | print()

--- uppercase.py
+++ uppercase.py
@@ -3,3 +3,7 @@ print(f"After:  {before.upper()}")
 
 # f-string (formatted string literal)
 # which allows you to embed expressions inside string literals using {}.
+#Another way of doing is with a Named Parameter (end=""):
+#before = input("Before: ")
+#print(After: " ", end ="")
+#print(before.upper())
